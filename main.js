ff26f5259cf8965edeb0e42d43b5d417bdff4813
KK Sri Lingaa <66474964+Lee-codee-159@users.noreply.github.com>
2021-09-04 13:55:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(350, 300);
+    canvas.center();
+}
+function draw(){
+    
+}
+function saveimage(){
+    save("Myselfie.png");
+}
